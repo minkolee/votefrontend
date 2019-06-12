fa4f29683c4a147752f95fc9cb924e1f3dff4c0a
minkolee <minkolee@gmail.com>
2019-06-12 20:18:03 +0800

    push 一下晚上 继续

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <template>
-  <v-app>
-    <v-container fill-height  grid-list-xs text-xs-center>
-      <router-view xs12 md6></router-view>
-      <router-view xs12 md6></router-view>
-    </v-container>
-
-  </v-app>
+      <router-view></router-view>
 </template>
 
 <script>
-
 export default {
   name: 'App',
   components: {
