572e132fefda81df76c5fb3a4d9b6c4282bc3450
minkolee <minkolee@gmail.com>
2019-06-17 16:58:23 +0800

    写好了投票作业 编程入门了

--- src/components/Vote.vue
+++ src/components/Vote.vue
@@ -1,27 +1,33 @@
 <template>
     <v-container>
-        <v-layout v-if="voted" justify-center>
+        <v-layout v-if="voted" align- justify-center>
             <v-flex xs12 class="text-xs-center">
                 <h2 class="display-3">投票结果</h2>
             </v-flex>
         </v-layout>
         <v-layout>
             <v-flex xs12 class="text-xs-center">
-                <h3 class="display-2" v-if="this.expire-(new Date()).getTime()>=0">还有{{day}}天{{hr}}小时{{min}}分钟{{sec}}秒投票截止</h3>
+                <h3 class="body-1 fixed" v-cloak v-if="this.expire-(new Date()).getTime()>=0">投票截止<br>{{day}}:{{hr}}:{{min}}:{{sec}}</h3>
+                <h3 class="body-1 fixed" v-cloak v-if="this.expire-(new Date()).getTime()<0">投票已截止</h3>
             </v-flex>
         </v-layout>
+        <v-layout justify-center>
+            <v-flex text-xs-center>
+                <v-chip>总投票人数{{vote.totalVotes}}人</v-chip>
+            </v-flex>
+        </v-layout>
+
         <v-layout v-if="!voted">
             <v-flex xs12 class="text-xs-center">
                 <h2 class="display-3">请先投票然后查看投票结果</h2>
             </v-flex>
         </v-layout>
-        <v-layout v-for="(voteItem, index) in vote.votes" :key="index" justify-center align-center wrap>
-            <v-flex xs12 sm3 md2 class="pa-0 ma-0">
+        <v-layout v-for="(voteItem, index) in voteWithPercent" :key="index" justify-center align-center wrap>
+            <v-flex xs12 sm12 md3 class="pa-0 ma-0">
                 <v-layout justify-end>
-                    <v-flex offset-xs-1>
-                        <label v-if="voted">{{voteItem.name}}&nbsp;{{voteItem.score}}票
+                    <v-flex text-xs-left mr-3 text-sm-left text-md-right>
+                        <label v-if="voted"><span class="body-1 ma-1">{{voteItem.name}}</span><span class="ma-1 primary--text">{{voteItem.score}}票</span><span class="ma-1 error--text">{{((voteItem.percent)*100).toFixed(2)}}</span>%
                             <input type="checkbox" v-if="voted" v-model="votenames" :value="voteItem.name" >
-
                         </label>
                         <label v-if="!voted">{{voteItem.name}}
                             <input type="checkbox" v-if="!voted" v-model="votenames" :value="voteItem.name" >
@@ -29,16 +35,17 @@
                     </v-flex>
                 </v-layout>
             </v-flex>
-            <v-flex xs11 sm6 md7>
+            <v-flex xs12 sm12 md8>
                 <v-progress-linear
                         v-if="voted"
-                        color="red"
+                        :color="voteItem.color"
                         height="10"
-                        :value="voteItem.score"
-                        background-color="rgba(250,250,250,0)"
+                        :value="voteItem.percent*100"
+                        background-color="rgba(230,230,230,0.6)"
                 ></v-progress-linear>
             </v-flex>
         </v-layout>
+
         <v-layout justify-center>
             <v-btn color="primary" @click="comeToVote">投票</v-btn>
         </v-layout>
@@ -77,7 +84,25 @@
                 votenames:[],
                 errormessage: "",
                 errorOccured: false,
-                day: 0, hr: 0, min: 0, sec: 0
+                day: 0, hr: 0, min: 0, sec: 0,
+                colors:{
+                    color0:'#FB8C00',
+                    color1: '#D32F2F',
+                    color2: '#D81B60',
+                    color3: '#8E24AA',
+                    color4: '#5E35B1',
+                    color5: '#3949AB',
+                    color6: '#1E88E5',
+                    color7: '#039BE5',
+                    color8: '#00ACC1',
+                    color9: '#00897B',
+                    color10: '#43A047',
+                    color11: '#7CB342',
+                    color12: '#7CB342',
+                    color13: '#C0CA33',
+                    color14: '#FFB300',
+                    color15: '#F4511E',
+                }
             }
         },
         computed:{
@@ -89,32 +114,50 @@
                 this.votenames.map(votename => temp.push(
                     new VoteItem(votename, 0)
                 ));
-
                 var o = {};
                 o.votes = temp;
                 return o;
             },
+            voteWithPercent: function () {
+                let votes = this.vote.votes;
+                if (votes!== undefined) {
+                    votes.forEach((voteItem, index) =>{
+                        if (this.vote.totalVotes <= 0) {
+                            voteItem.percent = 0
+                        } else {
+                            voteItem.percent = voteItem.score / this.vote.totalVotes
+                            voteItem.color = this.colors['color'+index % 16];
+                        }
+                    });
+                } else {
+                    votes = []
+                }
+
+                return votes
+            }
         },
         methods: {
             comeToVote: function () {
                 // eslint-disable-next-line
-                console.log("当前的votes对象是" + this.vote)
+                // console.log(this.voteWithPercent)
                 // eslint-disable-next-line
-                console.log('发送的投票对象是' + JSON.stringify(this.voteSentToBackend))
+                // console.log("当前的votes对象是" + this.vote)
+                // eslint-disable-next-line
+                // console.log('发送的投票对象是' + JSON.stringify(this.voteSentToBackend))
                 axios.post('http://localhost:8080/api/vote', this.voteSentToBackend, {
                     headers: {
                         Authorization: this.$store.state.token
                     }
                 }).then(res => {
                     // eslint-disable-next-line
-                    console.log(res)
+                    // console.log(res)
                     this.$emit('new-voted', res.data);
                 }).catch((err) => {
                     // eslint-disable-next-line
                     if (err.response.status === 406) {
                         this.errorOccured = true;
                         // eslint-disable-next-line
-                        console.log(this.errorOccured)
+                        // console.log(this.errorOccured)
                     } else {
                         this.$router.push('/login')
                     }
@@ -138,6 +181,10 @@
         },
         mounted() {
             this.countdown();
+            let _this = this;
+            setInterval(function () {
+                _this.$emit('new-voted',0)
+            }, 20000);
         }
     }
 </script>
